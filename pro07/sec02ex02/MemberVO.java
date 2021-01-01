1e3d3d859936d2b46d5517a704cdbd4cf3eca64d
TaeHyeon Kim <kth990303@naver.com>
2021-01-01 14:47:36 +0900

    addMember, delMember
    내 입맛에 맞게 수정~

---
+++ pro07/sec02ex02/MemberVO.java
@@ -0,0 +1,47 @@
+package sec02.ex02;
+
+import java.sql.Date;
+
+public class MemberVO {
+	private String id;
+	private String pwd;
+	private String tier;
+	private Date joinDate;
+	
+	public MemberVO() {
+		System.out.println("MemberVO 호출");
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getTier() {
+		return tier;
+	}
+
+	public void setTier(String tier) {
+		this.tier = tier;
+	}
+
+	public Date getJoinDate() {
+		return joinDate;
+	}
+
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
+
+}
